7cfc13864deb9752206118a535998ac7a14d4b2a
kamran <ksf365@gmail.com>
2024-12-20 15:42:08 +0530

    adding

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,21 +6,21 @@ export default defineConfig({
   plugins: [react()],
 }) */
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// Correctly resolve __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': `${__dirname}/src`, // Example alias for the `src` folder
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+  import { fileURLToPath } from 'url';
+  import { dirname } from 'path';
+  
+  // Correctly resolve __dirname and __filename
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  
+  export default defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': `${__dirname}/src`, // Example alias for the `src` directory
+      },
     },
-  },
-});
-
+  });
+  
