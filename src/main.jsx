3722a2201355988b4e088a90f64b76fde711bc7a
kamran <ksf365@gmail.com>
2024-12-20 15:54:38 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-/*import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
@@ -7,22 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-) */
-
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react';
-  import { fileURLToPath } from 'url';
-  import { dirname } from 'path';
-  
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  
-  export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': `${__dirname}/src`,
-      },
-    },
-  });
-  
+)
