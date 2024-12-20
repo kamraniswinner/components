beddfdd08da4b5d5683ee3f23580a2bf7b0f5d36
kamran <ksf365@gmail.com>
2024-12-20 15:38:43 +0530

    adding

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+}) */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Correctly resolve __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': `${__dirname}/src`, // Example alias for the `src` folder
+    },
+  },
+});
+
